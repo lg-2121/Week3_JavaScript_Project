1f7cace7b0edf9c5903c4b72db804a5b9a251d7b
lgLark <lgray2121@gmail.com>
2023-10-23 21:34:12 -0400

    updated linking path in html for css and JavaScript files to work properly. Added variables for the password to pick and choosefrom and added a window prompt for the character length.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// All password criteria variables
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var numbers = "0123456789";
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +19,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+  var passwordLength = window.prompt("How many characters would you like your password to contain?");
+  
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Your password must be between 8 and 128 characters.");
+    var passwordLength = prompt("How many characters would you like your password to contain?");
+
+
+  }
+}
